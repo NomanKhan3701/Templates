d4d8a9f43cb31a4369f08ebd86f48d812f765830
NomanKhan3701 <noman.khan3701@gmail.com>
2023-01-26 21:10:37 +0530

    Updated the absolute imports

---
+++ NextJS/Next13Template/pages/_app.jsx
@@ -0,0 +1,33 @@
+import { store } from '@store/configureStore';
+import Layout from '@Container/Layout/Layout';
+import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'public/assets/styles/globals.scss';
+import 'public/assets/styles/utils.scss';
+import 'public/assets/styles/font.scss';
+
+function App({ Component, pageProps }) {
+
+  return (
+    <Provider store={store}>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  )
+}
+
+export default App;
---
+++ NextJS/Next13Template/pages/login/index.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Link from 'next/link';
+import { toast } from 'react-toastify'
+import *  as actions from '@actions/index';
+import styles from './Login.module.scss';
+
+const Login = () => {
+	const loginProgress = useSelector((state) => state.auth.loginInProgress);
+	const dispatch = useDispatch();
+	const [loginData, setLoginData] = useState({
+		email: "",
+		password: "",
+	});
+
+	const Login = () => {
+		dispatch(actions.login({
+			email: loginData.email,
+			password: loginData.password,
+		}));
+	}
+
+	const loginInputHandler = (event) => {
+		const { name, value } = event.target;
+		setLoginData((prevData) => {
+			return { ...prevData, [name]: value };
+		});
+	}
+
+	return (
+		<div className={styles.Login + " container padding_top_nav"}>
+			<div className={styles.login_container}>
+				<h1>Welcome back to Artex</h1>
+				<div className={styles.sub_util}>
+					Don't have an account? <Link href='/signup'><span>Signup</span></Link>
+				</div>
+				<div className={styles.form_container}>
+					<div className={styles.input_container}>
+						<label htmlFor="email">Email</label>
+						<input type="text" name='email' value={loginData.email} onChange={loginInputHandler} />
+					</div>
+					<div className={styles.input_container}>
+						<label htmlFor="password">Password</label>
+						<input type="password" name='password' value={loginData.password} onChange={loginInputHandler} />
+					</div>
+				</div>
+
+				<div className={styles.login_btn} name="login_submit" onClick={Login}>Login</div>
+			</div>
+
+			{/* <div className={styles.login_image">
+				<img src="/public/assets/images/loginImage.svg" alt="" />
+			</div> */}
+		</div>
+	)
+}
+
+export default Login
