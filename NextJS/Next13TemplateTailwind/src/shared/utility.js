fb1fa3b804e7e91e6c4cd95b9eb4ff9ce20d9bf9
NomanKhan3701 <noman.khan3701@gmail.com>
2023-01-26 22:12:09 +0530

    nextjs tailwind

--- NextJS/Next13TemplateTailwind/src/shared/utility.js
+++
@@ -1,7 +0,0 @@
-export const updateObject = (oldObject, updatedProperties) => {
-	const updated = {
-		...oldObject,
-		...updatedProperties,
-	};
-	return updated;
-};
--- NextJS/Next13TemplateTailwind/src/store/actions/index.js
+++
@@ -1,9 +0,0 @@
-export {
-	authCheckState,
-	authStart,
-	authSuccess,
-	authFail,
-	logout,
-	login,
-	signup,
-} from './auth';
