ac14ad833679e76cd4c602220ca19613b168e53c
NomanKhan3701 <noman.khan3701@gmail.com>
2023-01-26 20:37:01 +0530

    First commit

---
+++ Next13Template/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Navbar.module.scss';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+	return (
+		<div className={styles.Navbar}>Navbar</div>
+	)
+}
+
+export default Navbar
